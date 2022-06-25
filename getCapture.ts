dbaedbf0848a356ae99822e7eed60c283df6bc2a
alivar391 <patriot391@mail.ru>
2022-06-25 12:00:11 +0300

    feat: add color correction to screenshot function

--- getCapture.ts
+++ getCapture.ts
@@ -3,15 +3,25 @@ import Jimp from "jimp";
 
 export const getCapture = async () => {
   const mouse = robot.getMousePos();
-  const bitMap = robot.screen.capture(
-    mouse.x - 200,
-    mouse.y - 200,
-    200 * 2,
-    200 * 2,
-  );
-  const img = new Jimp(200 * 2, 200 * 2);
+  const bitMap = robot.screen.capture(mouse.x - 100, mouse.y - 100, 200, 200);
+  const img = new Jimp(200, 200);
 
-  img.bitmap.data = bitMap.image;
+  let red: number, green: number, blue: number;
+  bitMap.image.forEach((byte: number, i: number) => {
+    switch (i % 4) {
+      case 0:
+        return (blue = byte);
+      case 1:
+        return (green = byte);
+      case 2:
+        return (red = byte);
+      case 3:
+        img.bitmap.data[i - 3] = red;
+        img.bitmap.data[i - 2] = green;
+        img.bitmap.data[i - 1] = blue;
+        img.bitmap.data[i] = 255;
+    }
+  });
 
   const base64 = await img.getBufferAsync(Jimp.MIME_PNG);
 
